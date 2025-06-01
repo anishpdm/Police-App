7e7357990ae8f9ed6aa64af1c097aa06f941bb35
anishpdm <anish.vilayil.s@gmail.com>
2025-06-01 09:44:36 +0530

    new

--- test.php
+++ test.php
@@ -1,19 +1,1 @@
-<?php
-
 <?php phpinfo(); ?>
-
-
-echo password_hash("admin12345", PASSWORD_DEFAULT);
-
-
-$plain = 'admin12345';
-$hash = '$2y$10$TDdjMBN2xMNq5.9J185V4.4mG0DnXGtrvuSrpKG7KORs0LjPmlb8q'; // paste actual DB hash
-
-if (password_verify($plain, $hash)) {
-    echo "✅ Match";
-} else {
-    echo "❌ No match";
-}
-
-
-?>
